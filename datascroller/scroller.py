7bd89cb1dd9c8ba99323fc44084c7d111671ee8c
Ben Ogorek <baogorek@gmail.com>
2019-08-16 03:24:22 -0700

    scroller boundaries working

--- datascroller/scroller.py
+++ datascroller/scroller.py
@@ -1,14 +1,10 @@
 import os
 import curses
-
-import statsmodels as sm
-import statsmodels.api as smapi
+import pandas as pd
 
 # Alternate names:
 # scrolldf
 # pdscroller
-
-iris = smapi.datasets.get_rdataset('iris').data
 
 def get_key_and_print(stdscr, disp_str):
     curses.curs_set(0)
@@ -21,28 +17,43 @@ def get_key_and_print(stdscr, disp_str):
             break
     return key
 
-def scroller(df): # ignores df for now. Uses iris
+def scroller(df, width=5, height=8): # ignores df for now. Uses iris
     # Tunable Params
     # ?
     # Initialization
+    last_row = df.shape[0] - 1
+    last_col = df.shape[1] - 1
+    print(last_col)
+
+    # Initializing the window
     start_row = 0
-    end_row = min(5, df.shape[0])
+    end_row = min(height, last_row)
     start_col = 0
-    end_col = min(3, df.shape[1])
+    end_col = min(width, last_col)
+
     key = -99
     # The scroller loop
     while key not in [10, 113]: # Enter or 'q' quits (on my machine at least)
         df_window = df.iloc[start_row:end_row, start_col:end_col]
         key = curses.wrapper(get_key_and_print, df_window.to_string())
-        if key == 104:
+        if key == 104 and start_col > 0: # h - vim key for left
             start_col -= 1
             end_col -= 1
-        elif key == 106:
+        elif key == 108 and start_col + width <= last_col: # l - vim key for right
             start_col += 1
             end_col += 1
+        elif key == 106 and start_row + height <= last_row: # j - vim key for down
+            start_row += 1
+            end_row += 1
+        elif key == 107 and start_row > 0: # k - vim key for up
+            start_row -= 1
+            end_row -= 1
+       # j is down: 106, k is up, 107, h is left, 104, l is right - 108
     return df_window
 
-scroller(iris) # Start by pressing the "j" key a couple of times, then press "h"
+train = pd.read_csv('train.csv')
+
+scroller(train.tail(50)) # Start by pressing the "j" key a couple of times, then press "h"
 
 if __name__ == "__main__":
     print("Run scroller(df) in ipython")
