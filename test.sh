04182cfed4c29199067e5a85a0dadf5e0297d988
Kevin Merfeld <kevin.merfeld@zoro.com>
2019-12-16 10:16:23 -0600

    adding linting

---
+++ test.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+function install_test_requirements(){
+    pip install -r ./test_requirements.txt
+}
+
+function run_test(){
+
+    flake8 \
+    --max-line-length 160 \
+    ./datascroller
+}
+
+main() {
+    install_test_requirements
+    run_test
+}
+
+main
