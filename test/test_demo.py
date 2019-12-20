7e41035cd603938af73ce1108431cceacc0f73e5
Kevin Merfeld <kevin.merfeld@zoro.com>
2019-12-20 17:29:54 -0600

    reversing last one

--- test/test_demo.py
+++ test/test_demo.py
@@ -6,10 +6,7 @@ from datascroller import demo
 
 
 def test_read_titanic():
-    """
-    Simple function to test demo.read_titanc()
-    Test passes if demo.read_titanc() can read titanic.csv into a dataframe
-    """
+
     csv_path = os.path.join(os.getcwd(), 'datascroller/demo_data/titanic.csv')
     control_df = pd.read_csv(csv_path)
     test_df = demo.read_titanic()
