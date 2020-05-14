47e044e40361017875dcd65bdfc58942ae6d78fe
John C. Merfeld <john.merfeld@gmail.com>
2020-05-14 15:51:07 -0400

    updated minor version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ For more info, please see our github page: github.com/baogorek/datascroller
 setup(
     name='datascroller',
     packages=['datascroller'],
-    version='1.3.3',
+    version='1.4.0',
     license='MIT',
     description='Data scrolling in the terminal',
     long_description=LONG_DESCRIPTION,
