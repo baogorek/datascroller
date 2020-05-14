43b08b7b28fcea23385b36a7c121874798cb1034
John C. Merfeld <john.merfeld@gmail.com>
2020-05-14 16:00:40 -0400

    added parquet test

--- datascroller/demo.py
+++ datascroller/demo.py
@@ -5,5 +5,8 @@ import pandas as pd
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'demo_data')
 
 
-def read_titanic():
+def read_titanic_csv():
     return pd.read_csv(os.path.join(DATA_DIR, 'titanic.csv'))
+
+def read_titanic_parquet():
+    return pd.read_parquet(os.path.join(DATA_DIR, 'titanic.parquet'))
--- test/test_demo.py
+++ test/test_demo.py
@@ -8,10 +8,16 @@ from datascroller import demo
 class DataTests(unittest.TestCase):
     """Tests for ensuring that data can be correctly loaded"""
 
-    def test_read_titanic(self):
+    def test_read_csv(self):
         csv_path = os.path.join(os.getcwd(), 'datascroller/demo_data/titanic.csv')
         control_df = pd.read_csv(csv_path)
-        test_df = demo.read_titanic()
+        test_df = demo.read_titanic_csv()
+        self.assertTrue(control_df.equals(test_df))
+
+    def test_read_parquet(self):
+        parquet_path = os.path.join(os.getcwd(), 'datascroller/demo_data/titanic.csv')
+        control_df = pd.read_parquet(parquet_path)
+        test_df = demo.read_titanic_parquet()
         self.assertTrue(control_df.equals(test_df))
 
 
